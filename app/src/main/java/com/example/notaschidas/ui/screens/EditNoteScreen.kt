6a9ff007e5abbc1ec4efed740d4b34defe64576a
Sam6482 <20243ds141@utez.edu.mx>
2025-12-09 17:48:53 -0600

    Importaciones necesarias

--- app/src/main/java/com/example/notaschidas/ui/screens/EditNoteScreen.kt
+++ app/src/main/java/com/example/notaschidas/ui/screens/EditNoteScreen.kt
@@ -1,12 +1,30 @@
 package com.example.notaschidas.ui.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import coil.compose.AsyncImage
 import java.io.File
 
 @Composable
