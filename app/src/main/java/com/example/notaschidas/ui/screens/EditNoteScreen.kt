769d967d359d3034732eb537d1eaf67e044f6d98
Sam6482 <20243ds141@utez.edu.mx>
2025-12-09 17:37:19 -0600

    Importe lo necesario para que funcione todo en EditNoteScreen.kt

--- app/src/main/java/com/example/notaschidas/ui/screens/EditNoteScreen.kt
+++ app/src/main/java/com/example/notaschidas/ui/screens/EditNoteScreen.kt
@@ -1,5 +1,33 @@
 package com.example.notaschidas.ui.screens
 
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import coil.compose.AsyncImage
+
+import java.io.File
+import java.util.jar.Manifest
+
+
 @Composable
 fun AddNoteScreen(
     onGuardar: (String, String, Uri?) -> Unit,
