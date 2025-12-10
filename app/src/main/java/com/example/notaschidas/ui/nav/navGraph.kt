98349f75a541d474bc1481c100d7f065b2ec50ad
Max-Mtzz <20243ds119@utez.edu.mx>
2025-12-09 19:45:47 -0600

    Agregue la importacion de las rutas

--- app/src/main/java/com/example/notaschidas/ui/nav/navGraph.kt
+++ app/src/main/java/com/example/notaschidas/ui/nav/navGraph.kt
@@ -3,6 +3,7 @@ package com.example.notaschidas.ui.nav
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.notaschidas.ui.screens.AddNoteScreen
