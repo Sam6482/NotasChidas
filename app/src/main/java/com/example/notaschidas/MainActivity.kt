ee5f81dd608d2e9232d7dd94593bbbac81875a74
Max-Mtzz <20243ds119@utez.edu.mx>
2025-12-09 19:48:26 -0600

    agregue la navegacion al main

--- app/src/main/java/com/example/notaschidas/MainActivity.kt
+++ app/src/main/java/com/example/notaschidas/MainActivity.kt
@@ -11,7 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.notaschidas.ui.nav.NavGraph
 import com.example.notaschidas.ui.theme.NotasChidasTheme
+import com.example.notaschidas.viewmodel.NoteViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NotasChidasTheme {
+                val noteViewModel: NoteViewModel = viewModel()
 
+                NavGraph(viewModel = noteViewModel)
             }
         }
     }
